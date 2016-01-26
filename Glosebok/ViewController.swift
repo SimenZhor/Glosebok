9990c25575a3d292f7c2f47030678c7a98368275
Simen E. Sørensen <Simen@simens-mac.local>
2016-01-25 22:36:30 -0800

    Initial Commit

---
+++ Glosebok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Glosebok
+//
+//  Created by Simen E. Sørensen on 25/01/16.
+//  Copyright © 2016 Simen E. Sørensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
