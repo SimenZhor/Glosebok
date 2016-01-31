1ad40a5b9a18927de0c7b08f8069930c569ca9cd
Simen E. Sørensen <Simen@simens-mac.local>
2016-01-31 05:03:07 -0800

    Initialising a new tableview class

---
+++ Glosebok/GlosebokTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GlosebokTableViewCell.swift
+//  Glosebok
+//
+//  Created by Simen E. Sørensen on 31/01/16.
+//  Copyright © 2016 Simen E. Sørensen. All rights reserved.
+//
+
+import UIKit
+
+class GlosebokTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
