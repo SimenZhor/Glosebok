7834054da64cb10b8eee83f1a5db471564892927
Simen E. Sørensen <Simen@Simens-Mac.local>
2016-02-08 11:45:16 -0800

    Glosebok now conforms to NSObject/NSCoding to make it saveable with NSUserDefaults

--- Glosebok/Glosebok.swift
+++ Glosebok/Glosebok.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Glosebok{
+class Glosebok: NSObject, NSCoding{
     
     //MARK: Properties
 
@@ -17,13 +17,25 @@ class Glosebok{
     var lang2: String
     
     var currentStatus: UIImage
-    var wordsLang1: [String]
-    var wordsLang2: [String]
     var glossary: [[String]] //glossary = [wordsLang1,wordsLang2]
     var translatedWords: [[String]]
     var overallRating: Double = 0.0
     var ratingForEachWord: [Int] = [Int]()
     var smileys = [UIImage]()
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let titleKey = "title"
+        static let lang1Key = "lang1"
+        static let lang2Key = "lang2"
+        static let currentStatusKey = "currentStatus"
+        static let glossaryKey = "glossary"
+        static let avgRatingKey = "avgRating"
+        static let ratingEachWordKey = "ratingForEachWord"
+        static let smileysKey = "smileys"
+        
+    }
     
     //MARK: Initialization
     init(title: String?, lang1: String, lang2: String){
@@ -36,6 +48,12 @@ class Glosebok{
         smileys.append(UIImage(named: "prettyGood")!)
         smileys.append(UIImage(named: "complete")!)
         
+        self.currentStatus = smileys[0]
+        
+        self.glossary = [[],[]]
+        self.translatedWords = [[],[]]
+        
+        
         
         if title == ""{
          self.title = ("Min Glosebok " + String(GlosebokTableViewController.bookCounter))
@@ -43,13 +61,28 @@ class Glosebok{
             GlosebokTableViewController.bookCounter+=1
         }
         
-        wordsLang1 = []
-        wordsLang2 = []
-        glossary = [wordsLang1,wordsLang2]
-        translatedWords = [[],[]]
-        currentStatus = smileys[0]
-        
     }
+    /*
+    convenience init?(title: String, lang1: String, lang2: String, currentStatus: UIImage, glossary: [[String]], overallRating: Double?, ratingForEachWord: [Int]?){
+        self.title = title
+        self.lang1 = lang1
+        self.lang2 = lang2
+        self.currentStatus = currentStatus
+        self.glossary = glossary
+
+        
+        if overallRating != nil{
+            self.overallRating = overallRating!
+        }
+        if ratingForEachWord != nil{
+            self.ratingForEachWord = ratingForEachWord!
+        }
+        
+        translatedWords = [[],[]]
+        
+        self.init(title: title, lang1: lang1, lang2: lang2)
+    }*/
+
     
     //MARK: Functions
     func updateStatus(currentStatus: UIImage?){
@@ -86,6 +119,41 @@ class Glosebok{
 
     }
     
+    //MARK: NSCoding
+    
+    /*
+    let titleKey = "title"
+    let lang1Key = "lang1"
+    let lang2Key = "lang2"
+    let currentStatusKey = "currentStatus"
+    let glossaryKey = "glossary"
+    let avgRatingKey = "avgRating"
+    let ratingEachWordKey = "ratingForEachWord"
+    let smileysKey = "smileys"*/
 
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(title, forKey: PropertyKey.titleKey)
+        aCoder.encodeObject(lang1, forKey: PropertyKey.lang1Key)
+        aCoder.encodeObject(lang2, forKey: PropertyKey.lang2Key)
+        aCoder.encodeObject(currentStatus, forKey: PropertyKey.currentStatusKey)
+        aCoder.encodeDouble(overallRating, forKey: PropertyKey.avgRatingKey)
+        
+        aCoder.encodeObject (glossary, forKey: PropertyKey.glossaryKey)
+        aCoder.encodeObject(ratingForEachWord, forKey: PropertyKey.ratingEachWordKey)
+        
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let title = aDecoder.decodeObjectForKey(PropertyKey.titleKey) as! String
+        let lang1 = aDecoder.decodeObjectForKey(PropertyKey.lang1Key) as! String
+        let lang2 = aDecoder.decodeObjectForKey(PropertyKey.lang2Key) as! String
+        
+        self.init(title: title, lang1: lang1, lang2: lang2)
+       
+        self.currentStatus = aDecoder.decodeObjectForKey(PropertyKey.currentStatusKey) as! UIImage
+        self.overallRating = aDecoder.decodeDoubleForKey(PropertyKey.avgRatingKey)
+        self.glossary = aDecoder.decodeObjectForKey(PropertyKey.glossaryKey) as! [[String]]
+        self.ratingForEachWord = aDecoder.decodeObjectForKey(PropertyKey.ratingEachWordKey) as! [Int]
+    }
     
 }
